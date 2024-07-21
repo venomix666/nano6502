c1179738b17db74d9c77412c16458a22018099b4
Henrik L <henrik.lofgren@gmail.com>
2024-07-21 14:33:20 +0200

    Working USB keyboard interface

--- romcode/uart.asm
+++ romcode/uart.asm
@@ -3,30 +3,46 @@ UART_tx_done    =     $fe01
 UART_rx_data    =     $fe02
 UART_rx_avail   =     $fe03
 
+keyb_data_avail =     $fe00
+keyb_data       =     $fe01
+
 TTY_busy        =     $fe07
 TTY_out         =     $fe06
 
 IO_bank         =     $00
 IO_bank_val     =     $01
+IO_bank_keyb    =     $06
 IO_bank_temp    =     $04
 
 IO_bank_tty     =     $04
 ;
 ; input chr from UART (waiting)
 ;
+; Also checks for USB keyboard data
+;
 UART_Input:	
     ; Preserve current IO bank
     lda IO_bank
     sta IO_bank_temp
-    ; Set IO bank
+UART_input_wait:
+    ; Set UART IO bank
     lda #IO_bank_val
     sta IO_bank
     ; Check if data is available
-UART_Input_wait:
     lda UART_rx_avail
-    beq UART_Input_wait    
-UART_Input_done:
+    beq UART_Input_keyb
     lda UART_rx_data
+    bne UART_Input_done
+UART_input_keyb: 
+    ; Set keyboard IO bank
+    lda #IO_bank_keyb
+    sta IO_bank
+    lda keyb_data_avail
+    beq UART_input_wait
+    lda keyb_data
+    bne UART_input_done
+    jmp UART_input_wait
+UART_input_done:
     pha
     lda IO_bank_temp
     sta IO_bank
@@ -34,6 +50,8 @@ UART_Input_done:
     rts
 ;
 ; non-waiting get character routine 
+;
+; Also checks for USB keyboard data
 ;
 UART_Scan:	
 	; Preserve current IO bank
@@ -44,8 +62,21 @@ UART_Scan:
     sta IO_bank
     clc
     lda UART_rx_avail 
-	beq UART_Scan_Done
+	beq UART_Scan_keyb
     lda UART_rx_data
+    pha
+    lda IO_bank_temp
+    sta IO_bank
+    pla
+    sec
+    rts
+UART_Scan_keyb:
+    lda #IO_bank_keyb
+    sta IO_bank
+    lda keyb_data_avail
+    beq UART_Scan_Done
+    lda keyb_data
+    beq UART_Scan_Done
     pha
     lda IO_bank_temp
     sta IO_bank
--- src/usb_hid_host/usb_interface.v
+++ src/usb_hid_host/usb_interface.v
@@ -1,22 +1,19 @@
 // USB HID interface for the nano6502
 //
-// Very rudimentary for now, add translation of keyboard to ASCII later...
-// Will latching of all data be necessary due to different clock domains?
+// Based on USB HID core by nand2mario
 //
 // Registers:
-// 00       -   Data available
-// 01       -   Key 1
-// 02       -   Key 2
-// 03       -   Key 3
-// 04       -   Key 4
-// 05       -   Key modifiers
-// 06       -   Mouse button
-// 07       -   Mouse dx
-// 08       -   Mouse dy
-// 09       -   {4'b0000, game_d, game_u, game_r, game_l}
-// 0a       -   {2'b00, game_sta, game_sel, game_y, game_x, game_b, game_a}
-// 0b       -   Device type - 0: no device, 1: keyboard, 2: mouse, 3: gamepad
-// 0c       -   Error signal 
+// 00       -   New key
+// 01       -   Keyboard character
+// 02       -   Key modifier
+// 03       -   Mouse button
+// 04       -   Mouse dx
+// 05       -   Mouse dy
+// 06       -   {4'b0000, game_d, game_u, game_r, game_l}
+// 07       -   {2'b00, game_sta, game_sel, game_y, game_x, game_b, game_a}
+// 08       -   New USB report 
+// 09       -   Device type - 0: no device, 1: keyboard, 2: mouse, 3: gamepad
+// 0a       -   Error signal 
 
 module usb_interface(
 	input               clk_i,
@@ -34,7 +31,7 @@ module usb_interface(
 );
 
 wire            [1:0]   typ;        // Device type
-wire                    report;     // Pulse after report - is this for all data or only new connection?
+wire                    report;     // Pulse after report
 wire                    conerr;     // Error signal
 wire            [7:0]   key_modifiers;
 wire            [7:0]   key1, key2, key3, key4;
@@ -44,6 +41,16 @@ wire    signed  [7:0]   mouse_dy;
 wire                    game_l, game_r, game_u, game_d;
 wire                    game_a, game_b, game_x, game_y, game_sel, game_sta;
 
+
+reg             [7:0]   key_reg, key_old;
+reg                     new_key;
+reg                     key_down;
+reg                     new_key_set;
+reg             [7:0]   mouse_btn_reg;
+reg     signed  [7:0]   mouse_dx_reg, mouse_dy_reg;
+reg             [7:0]   game_dir_reg;
+reg             [7:0]   game_btn_reg;
+
 reg             [7:0]   data_o_reg;
 reg                     report_reg;
 
@@ -51,19 +58,17 @@ reg                     report_reg;
 always @(*)
 begin
     case(reg_addr_i)
-        8'h00: data_o_reg = {7'd0, report_reg};
-        8'h01: data_o_reg = key1;
-        8'h02: data_o_reg = key2;
-        8'h03: data_o_reg = key3;
-        8'h04: data_o_reg = key4;
-        8'h05: data_o_reg = key_modifiers;
-        8'h06: data_o_reg = mouse_btn;
-        8'h07: data_o_reg = mouse_dx;
-        8'h08: data_o_reg = mouse_dy;
-        8'h09: data_o_reg = {4'b0000, game_d, game_u, game_r, game_l};
-        8'h0a: data_o_reg = {2'b00, game_sta, game_sel, game_y, game_x, game_b, game_a};
-        8'h0b: data_o_reg = {6'd0, typ};
-        8'h0c: data_o_reg = {7'd0, conerr};
+        8'h00: data_o_reg = {7'd0, new_key};
+        8'h01: data_o_reg = key_reg;
+        8'h02: data_o_reg = key_modifiers;
+        8'h03: data_o_reg = mouse_btn_reg;
+        8'h04: data_o_reg = mouse_dx_reg;
+        8'h05: data_o_reg = mouse_dy_reg;
+        8'h06: data_o_reg = game_dir_reg;
+        8'h07: data_o_reg = game_btn_reg;
+        8'h08: data_o_reg = {7'd0, report_reg};
+        8'h09: data_o_reg = {6'd0, typ};
+        8'h0a: data_o_reg = {7'd0, conerr};
         default:
         begin
             data_o_reg = 8'd0;
@@ -73,15 +78,51 @@ end
 
 assign data_o = data_o_reg;
 
+// Latch data when new USB report comes
+always @(posedge clkusb_i or negedge rst_n_i)
+begin
+    if(rst_n_i == 1'b0)
+    begin
+        key_reg <= 8'd0;
+        key_old <= 8'd0;
+        key_down <= 1'd0;
+        mouse_btn_reg <= 8'd0;
+        mouse_dx_reg <= 8'd0;
+        mouse_dy_reg <= 8'd0;
+        game_btn_reg <= 8'd0;
+        game_dir_reg <= 8'd0;
+        new_key_set <= 1'd0;
+    end
+    else if(report == 1'b1) 
+    begin
+        key_old <= key1;
+        if(key1 != key_old && !key_down) begin
+            key_reg <= scancode2char(key1, key_modifiers);
+            new_key_set <= 1'b1;
+            key_down <= 1'b1;
+        end
+        else key_down <= 1'b0;
+        mouse_btn_reg <= mouse_btn;
+        mouse_dx_reg <= mouse_dx;
+        mouse_dy_reg <= mouse_dy;
+        game_dir_reg <= {4'b0000, game_d, game_u, game_r, game_l};
+        game_btn_reg <= {2'b00, game_sta, game_sel, game_y, game_x, game_b, game_a};
+    end
+    else new_key_set <= 1'b0;
+end
+
 // Latch the report pulse and clear on read
 always @(posedge clk_i or negedge rst_n_i)
 begin
     if(rst_n_i == 1'b0)
     begin
         report_reg = 1'b0;
+        new_key = 1'b0;
     end
     else if(report == 1'b1) report_reg = 1'b1;
-    else if((usb_cs == 1'b1) && (reg_addr_i == 8'h00)) report_reg = 1'b0;
+    else if(new_key_set == 1'b1) new_key <= 1'b1;
+    else if((usb_cs == 1'b1) && (reg_addr_i == 8'h00)) new_key = 1'b0;
+    else if((usb_cs == 1'b1) && (reg_addr_i == 8'h08)) report_reg = 1'b0;
 end
 
 
@@ -116,3 +157,81 @@ usb_hid_host usb_hid_host_inst(
 );
 
 endmodule
+
+localparam [7:0] SHIFT_MASK = 8'b00100010;
+localparam [7:0] CTRL_MASK = 8'b00010001;
+function [7:0] scancode2char(input [7:0] scancode, input [7:0] modifiers); 
+    reg [7:0] a;
+    if (scancode >= 4 && scancode <= 29) begin   // a-z
+        if (modifiers == 0)
+            a = scancode - 4 + 97;             // a: 97, A: 65, ctrl+a: 1
+        else if ((modifiers & SHIFT_MASK) && (modifiers & ~SHIFT_MASK) == 0)
+            a = scancode - 4 + 65;
+        else if ((modifiers & CTRL_MASK) && (modifiers & ~CTRL_MASK)==0)
+            a = scancode - 3;
+    end else if (modifiers == 0) begin
+        case (scancode)
+            30: a = "1";
+            31: a = "2";
+            32: a = "3";
+            33: a = "4";
+            34: a = "5";
+            35: a = "6";
+            36: a = "7";
+            37: a = "8";
+            38: a = "9";
+            39: a = "0";
+            40: a = 13;       // enter
+            41: a = 27;         // esc
+            42: a = 8;          // backspace
+            43: a = 9;          // tab
+            44: a = 32;         // space
+            45: a = "-";        // -
+            46: a = "=";        // =
+            47: a = "[";        // [
+            48: a = "]";        // ]
+            49: a = "\\";       // \
+            50: a = "#";        // non-use # ~
+            51: a = ";";        // ;
+            52: a = "'";        // '
+            53: a = "`";        // `
+            54: a = ",";        // ,
+            55: a = ".";        // .
+            56: a = "/";        // /
+            57: ;               // caps lock
+        endcase
+    end if ((modifiers & SHIFT_MASK) && (modifiers & ~SHIFT_MASK) == 0) begin
+        // shift down
+        case (scancode)
+            30: a = "!";
+            31: a = "@";
+            32: a = "#";
+            33: a = "$";
+            34: a = "%";
+            35: a = "^";
+            36: a = "&";
+            37: a = "*";
+            38: a = "(";
+            39: a = ")";
+            40: a = 13;       // enter
+            41: a = 27;         // esc
+            42: a = 8;          // backspace
+            43: a = 9;          // tab
+            44: a = 32;         // space
+            45: a = "_";
+            46: a = "+";
+            47: a = "{"; 
+            48: a = "}";
+            49: a = "|";
+            50: a = "~";
+            51: a = ":";
+            52: a = "\"";
+            53: a = "~";
+            54: a = "<";
+            55: a = ">";
+            56: a = "?";
+            57: ;
+        endcase 
+    end 
+    scancode2char = a;
+endfunction
